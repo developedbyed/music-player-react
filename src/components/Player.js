2f7e348df8164f36258b494aee1d7476ac1b8fba
developedbyed <ed.magician@gmail.com>
2020-09-23 22:21:31 +0300

    fixed input slider

--- src/components/Player.js
+++ src/components/Player.js
@@ -22,14 +22,14 @@ const Player = ({ currentSong, songs, isPlaying, setIsPlaying }) => {
   };
   //State
   const [songInfo, setSongInfo] = useState({
-    currentTime: null,
-    duration: null,
+    currentTime: 0,
+    duration: 0,
   });
 
   const timeUpdateHandler = (e) => {
     const current = e.target.currentTime;
     const duration = e.target.duration;
-    console.log(e.target.duration);
+    console.log(e.target.currentTime);
     setSongInfo({ ...songInfo, currentTime: current, duration: duration });
   };
 
@@ -38,12 +38,23 @@ const Player = ({ currentSong, songs, isPlaying, setIsPlaying }) => {
       Math.floor(time / 60) + ":" + ("0" + Math.floor(time % 60)).slice(-2)
     );
   }
+  const dragHandler = (e) => {
+    console.log(e.target.value);
+    audioRef.current.currentTime = e.target.value;
+    setSongInfo({ ...songInfo, currentTime: e.target.value });
+  };
 
   return (
     <div className="player">
       <div className="time-control">
         <p>{getTime(songInfo.currentTime)}</p>
-        <input type="range" />
+        <input
+          value={songInfo.currentTime}
+          type="range"
+          max={songInfo.duration}
+          min={0}
+          onChange={dragHandler}
+        />
         <p>{getTime(songInfo.duration)}</p>
       </div>
       <div className="play-control">
