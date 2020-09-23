28291adac11d7eb5b45affe2e97f391098b89621
developedbyed <ed.magician@gmail.com>
2020-09-23 20:24:19 +0300

    styled song

--- src/App.js
+++ src/App.js
@@ -1,13 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles/app.scss";
 //Import Components
 import Player from "./components/Player";
 import Song from "./components/Song";
 
 function App() {
+  const [songs, setSongs] = useState([
+    {
+      name: "Beaver Creek",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg",
+      artist: "Aso, Middle School, Aviino",
+    },
+    {
+      name: "Daylight",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/07/ef95e219a44869318b7806e9f0f794a1f9c451e4-1024x1024.jpg",
+      artist: "Aiguille",
+    },
+    {
+      name: "Keep Going",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg",
+      artist: "Swørn",
+    },
+    {
+      name: "Nightfall",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/07/ef95e219a44869318b7806e9f0f794a1f9c451e4-1024x1024.jpg",
+      artist: "Aiguille",
+    },
+    { name: "Reflection", cover: "Sworn", artist: "Swørn" },
+    {
+      name: "Under the City Stars",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg",
+      artist: "Aso, Middle School, Aviino",
+    },
+  ]);
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+
   return (
     <div className="App">
-      <Song />
+      <Song currentSong={currentSong} />
       <Player />
     </div>
   );
--- src/components/Song.js
+++ src/components/Song.js
@@ -1,11 +1,14 @@
 import React from "react";
+import Aiguille from "../assets/covers/Aiguille.jpg";
+import CanaryForest from "../assets/covers/CanaryForest.jpg";
 
-const Song = () => {
+const Song = ({ currentSong }) => {
+  console.log(currentSong.cover);
   return (
     <div className="song-container">
-      <h1>Picture</h1>
-      <h2>Song Name</h2>
-      <h3>Artist</h3>
+      <img src={currentSong.cover} alt="" />
+      <h2>{currentSong.name}</h2>
+      <h3>{currentSong.artist}</h3>
     </div>
   );
 };
