bcbc0a867ed7f128ba845e61b4021aecf140d0eb
developedbyed <ed.magician@gmail.com>
2020-09-24 15:25:48 +0300

    fixed input style

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
         "https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg",
       artist: "Aso, Middle School, Aviino",
       audio: "https://mp3.chillhop.com/serve.php/?mp3=10075",
+      color: "#205950",
     },
     {
       name: "Daylight",
